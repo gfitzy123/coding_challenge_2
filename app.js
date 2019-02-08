239eaaaed858959a2582a39f980e5162618fc317
Garrett Fitzgerald <gfitzgerald123@gmail.com>
2019-02-08 08:34:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,26 +2,32 @@ import express from 'express';
 import db from './db';
 // Set up the express app
 const app = express();
+const PORT = 3000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`)
+});
 
 //post metric
 app.post('/metric/:key', (req, res) => {
-
   if (db[req.params.key]){
     //metric exists in db
     //so we must add value
 
     let newValue = db[req.params.key] + parseInt(req.query.value)
+    console.log('Adding value to metric: ', db[req.params.key], ' by ', req.query.value)
     db[req.params.key] = newValue
+    
   } else {
     //metric does not exist
     //so we add to db
 
-      let value = parseInt(req.query.value)
-      db[req.params.key] = value
+    let value = parseInt(req.query.value)
+    db[req.params.key] = value
   }
 
   //return empty object as response
+  console.log('DB contents have been updated to: ', db)
   res.status(200).send({})
 });
 
@@ -32,11 +38,4 @@ app.get('/metric/:key/sum', (req, res) => {
   } else {
     res.status(200).send({'value': db[req.params.key]})
   }
-
-});
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
 });
