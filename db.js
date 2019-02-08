94060b623ca1c0c53f72db68716ffd26cf8f5d96
Garrett Fitzgerald <gfitzgerald123@gmail.com>
2019-02-08 08:34:49 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,44 +1,44 @@
-import express from 'express';
-import db from './db';
-// Set up the express app
-const app = express();
+
+//our "database"
+
+let metrics =  {};
+
+//functions to reset datastore every hour
+let clearDatabase = function() {
+    console.log('Clearing database.')
+    metrics = {}
+}
+
+let timeToLive = function() {
+    //this will set the database to clear at every interval (hour)
+    clearDatabase()
+    let d = new Date();
+    let n = d.toLocaleTimeString();
+    let hourInMilliseconds = 1000 * 60 * 60
+
+    console.log('Will clear database in: ', hourInMilliseconds, '..milliseconds.')
+    setInterval(function(){ clearDatabase() },  hourInMilliseconds);
+}
+
+//create a date
+let tomorrow  = new Date();
+
+//if we are on the strike of the hour, clear the database
+if (tomorrow.getMinutes() === 0) {
+    timeToLive()
+} else {
+    //find the difference between now and the end of the hour
+    tomorrow.setHours(tomorrow.getHours() + 1);
+    tomorrow.setMinutes(0);
+    tomorrow.setSeconds(0);
+
+    //call the time to live function on the end of the hour
+    let difference = tomorrow - new Date();
+    console.log('Setting TTL on database to..', difference)
+    setTimeout(function(){ timeToLive() }, difference);
+    //setTimeout(function(){ timeToLive() }, differnece);
+}
 
 
-//post metric
-app.post('/metric/:key', (req, res) => {
 
-  if (db[req.params.key]){
-    //metric exists in db
-    //so we must add value
-
-    let newValue = db[req.params.key] + parseInt(req.query.value)
-    console.log('Adding value to metric: ', db[req.params.key], ' by ', req.query.value)
-    db[req.params.key] = newValue
-  } else {
-    //metric does not exist
-    //so we add to db
-
-      let value = parseInt(req.query.value)
-      db[req.params.key] = value
-  }
-
-  //return empty object as response
-  console.log('DB contents have been updated to: ', db)
-  res.status(200).send({})
-});
-
-// get all metrics
-app.get('/metric/:key/sum', (req, res) => {
-  if (db[req.params.key]){
-      res.status(500).send({'error': 'metric does not exist'})
-  } else {
-    res.status(200).send({'value': db[req.params.key]})
-  }
-
-});
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`)
-});
+export default metrics;
